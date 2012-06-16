477433f1b23305d0ac396d7aaee82dbb70575e17
Verdi Erel Ergün <vergun@gmail.com>
2012-06-16 13:24:05 -0700

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arcania::Application.config.secret_token = '80c130a9f4840a08bfecf8ef68cd28a955cc160fadf30c88a498ca1665cd2eda0e4b76277db3a7ef853260888a0e3375f309de68d0991cfdf9e3d9c48b536212'
